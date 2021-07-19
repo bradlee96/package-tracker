fc6b940580d11cc8eac8fd5ad456902043ce4d46
Brad <bradleeusa@gmail.com>
2021-07-19 13:27:37 -0500

    Got basic redirect/oauth working through nodejs, next to implement frontend/backend comms

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 // backend/app.js
 
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const url = require('url');
+const opn = require('open'); // get rid of this shit later npm
+const destroyer = require('server-destroy');
+
 const express = require('express');
 const cors = require('cors');
-const { OAuth2Client } = require('google-auth-library');
+const {google} = require('googleapis');
+const gmail = google.gmail('v1');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +18,6 @@ app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000']
 }));
-const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
 const PORT = process.env.PORT || 3001;
 
@@ -22,24 +29,132 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!!" });
 });
 
-app.post("/api/v1/auth/google", async (req, res) => {
-  console.log("Creating token");
-  console.log(req.body);
-  const { token } = req.body;
-
-  console.log("Verifying token");
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.REACT_APP_GOOGLE_CLIENT_ID
-  });
-  const { name, email, picture } = ticket.getPayload();
-
-  // const user = await db.user.upsert({
-  //   where: { email: email },
-  //   update: { name, picture },
-  //   create: { name, email, picture }
-  // });
-  
-  // res.status(201);
-  res.json(ticket.getPayload);
+app.get("/oauth2callback", (req, res) => {
+  res.json({ message: "Hello from server!!" });
 });
+
+
+
+
+// Google API
+
+/**
+ * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
+ */
+const keyPath = path.join(__dirname, 'oauth2.keys.json');
+let keys = {redirect_uris: ['']};
+if (fs.existsSync(keyPath)) {
+  keys = require(keyPath).web;
+}
+
+/**
+ * Create a new OAuth2 client with the configured keys.
+ */
+const oauth2Client = new google.auth.OAuth2(
+  keys.client_id,
+  keys.client_secret,
+  keys.redirect_uris[0]
+);
+
+/**
+ * This is one of the many ways you can configure googleapis to use authentication credentials.  In this method, we're setting a global reference for all APIs.  Any other API you use here, like google.drive('v3'), will now use this auth client. You can also override the auth client at the service and method call levels.
+ */
+google.options({auth: oauth2Client});
+
+/**
+ * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
+ */
+async function authenticate(scopes) {
+  return new Promise((resolve, reject) => {
+    // grab the url that will be used for authorization
+    const authorizeUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes.join(' '),
+    });
+    // Open an http server to accept the oauth callback. In this simple example, the
+    // only request to our webserver is to /oauth2callback?code=<code>
+    const server = http
+      .createServer(async (req, res) => {
+        try {
+          console.log(req.url);
+          if (req.url.indexOf('/oauth2callback') > -1) {
+            // acquire the code from the querystring, and close the web server.
+            console.log("0");
+            const qs = new url.URL(req.url, 'http://localhost:3000')
+              .searchParams;
+            console.log("A");
+            res.end('Authentication successful! Please return to the console.');
+            server.destroy();
+            const {tokens} = await oauth2Client.getToken(qs.get('code'));
+            oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
+            resolve(oauth2Client);
+            console.log("done");
+          }
+          console.log("B");
+        } catch (e) {
+          console.log("C");
+          reject(e);
+        }
+      })
+      .listen(3000, () => {
+        // open the browser to the authorize url to start the workflow
+        opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
+      });
+    destroyer(server);
+  });
+}
+
+async function runSample() {
+  gmail.users.labels.list({
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = res.data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+
+  // retrieve user profile
+  // const res = await people.people.get({
+  //   resourceName: 'people/me',
+  //   personFields: 'emailAddresses',
+  // });
+  // console.log(res.data);
+}
+
+const scopes = [
+  'https://www.googleapis.com/auth/gmail.readonly',
+];
+authenticate(scopes)
+  .then(client => runSample(client))
+  .catch(console.error);
+
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+ function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.labels.list({
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = res.data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+}
--- frontend/src/components/pages/Services.js
+++ frontend/src/components/pages/Services.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import '../../App.css';
-import GoogleLogin from 'react-google-login';
 
 export default function Services() {
   const [data, setData] = useState(null);
@@ -11,33 +10,8 @@ export default function Services() {
     .then((data) => setData(data.message));
   }, [])
 
-  const handleLogin = async googleData => {
-    console.log(googleData);
-    console.log(googleData.tokenId);
-
-    const res = await fetch("http://localhost:3001/api/v1/auth/google", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ 
-        token: googleData.tokenId
-      })
-    });
-
-    const data = await res.json();
-    console.log(data); // do something with this data
-  }
-
   return (
     <div>
-      <GoogleLogin
-        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-        buttonText="Sign in with Google"
-        onSuccess={handleLogin}
-        onFailure={handleLogin}
-        cookiePolicy={'single_host_origin'}
-      />
       <h1 className='services'>Test</h1>
       <p>{!data ? "Loading" : data}</p>
     </div>
