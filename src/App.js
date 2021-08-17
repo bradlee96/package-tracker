ee544c2c8331e754d3ffff9029fc04389a34e2d7
Brad <bradleeusa@gmail.com>
2021-08-16 23:01:30 -0500

    Improved algorithm for searching emails

--- src/App.js
+++ src/App.js
@@ -102,8 +102,9 @@ export default function App() {
     // Set query string
     // const queryString = `package OR shipping OR shipped OR tracking before:${tomorrow} -return`
     const queryStrings = [
-      // `package OR shipping OR shipped OR tracking -return after:${certainDaysAgo} before:${tomorrow}`,
-      `in:all package OR shipping OR shipped OR tracking -return after:${certainDaysAgo} before:${tomorrow}`
+      // `in: all package OR shipping OR shipped OR tracking -return after:${certainDaysAgo} before:${tomorrow}`
+      // `in:all ("tracking number") OR (tracking AND (DHL OR FedEx OR UPS OR USPS OR "United States Postal Service")) -return after:${certainDaysAgo} before:${tomorrow}`
+      `in:all ("tracking number") OR (tracking AND (DHL OR FedEx OR UPS OR USPS OR "United States Postal Service")) -return`
     ]
 
     var threadIds = []
